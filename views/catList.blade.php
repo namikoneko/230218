2c53629fd543908613f31f8dc6437d99f22db706
yaguchi <yaguchi1129@gmail.com>
2023-03-04 12:27:38 +0900

    230304 1230

--- views/catList.blade.php
+++ views/catList.blade.php
@@ -4,20 +4,30 @@
 
     <div class="bg-white">
 
+    <div class="mt-2 px-3 py-2 row border-bottom">
+
+        <div class="col-1">id</div>
+        <div class="col-3">title</div>
+        <div class="col-4">url</div>
+        <div class="col-2">update</div>
+        <div class="col-2">delete</div>
+
+    </div>
+
     <div class="mt-2 px-3 py-2 row">
 
   @foreach($rows as $row)
 
     <div class="col-1">
-        id: {{$row["id"]}}
+        {{$row["id"]}}
     </div>
 
     <div class="col-3">
-        title: {{$row["title"]}}
+        {{$row["title"]}}
     </div>
 
     <div class="col-4 text-break">
-        url: {{$row["url"]}}
+        {{$row["url"]}}
     </div>
 
     <div class="col-2">
